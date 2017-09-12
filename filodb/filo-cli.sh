4db8141b76934a38e77d531eb57eadbe82084f35
zqhxuyuan <qihuang.zheng@fraudmetrix.cn>
2017-09-12 18:33:19 +0800

    filodb support

--- filodb/filo-cli.sh
+++ filodb/filo-cli.sh
@@ -1,8 +1,5 @@
 #!/usr/bin/env bash
-
-# set -x
-SCALA_VERSION="2.11"
-CLI_FILE=`pwd`"/cli/target/filo-cli.jar"
+CLI_FILE=`pwd`"/cli/bin/cli-main.sh"
 
 if [ ! -f "$CLI_FILE" ];then
     mvn package -Dmaven.test.skip=true
